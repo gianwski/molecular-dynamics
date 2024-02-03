a946268c8a7278cbaa687659c791b7686889ce75
Gianluca Regni <gianluca.regni@gmail.com>
2024-02-03 16:38:27 +0100

    Add files via upload

---
+++ MD.f90
@@ -0,0 +1,203 @@
+  program md
+
+  use utilities
+
+  implicit none
+
+! Parameter for unit conversions to atomic unit
+
+  double precision, parameter  :: kcal_to_Hartree = 0.0015936015d0
+  double precision, parameter  :: ev_to_Hartree = 0.0367492929d0
+  double precision, parameter  :: ang_to_bohr = 1.8897261d0
+  double precision, parameter  :: fs_to_timeau = 41.34137314d0
+  double precision, parameter  :: amu_to_au = 1822.8884850d0
+
+! double precision, parameter  :: Hartree_to_kcal = 627.50947d0
+! double precision, parameter  :: Hartree_to_ev = 27.211407953d0
+! double precision, parameter  :: bohr_to_ang = 0.52917721d0
+  double precision, parameter  :: timeau_to_fs = 0.02418884254d0
+! double precision, parameter  :: au_to_amu = 0.00054857990943d0
+
+! Definition of variables
+
+  integer                                        :: i, j, k, l, step, nstep, natom
+  double precision                               :: timestep, mass, eps, sig, kin_ener, pot_ener, energy, time
+  double precision, allocatable, dimension(:,:)  :: positions, velocities, distances, forces
+  character(len=100)                             :: filename = "input" ! Input file name
+  character(len=2), allocatable, dimension(:)    :: atom
+
+
+! Open output file for trajectory and energies
+
+  open(10, file="md.xyz")
+  open(11, file="energy.dat")
+
+! Read input variables
+
+  call lecture(filename, positions, atom, velocities, nstep, mass, timestep, sig, eps, natom)
+
+! Allocate memory
+
+  allocate(forces(natom,3), distances(natom,natom))
+
+! Write input parameters
+  
+  write(6,"(A)") "Initial parameters"
+  write(6,*)
+  write(6,"(A,I4)") "number of steps : ", nstep
+  write(6,"(A,F6.3)") "time step (fs)  : ", timestep
+  write(6,"(A,I2)") "number of atoms : ", natom
+  write(6,"(A,F9.6)") "masses (amu)    : ", mass
+  write(6,"(A,F9.6)") "epsilon (eV)   : ", eps 
+  write(6,"(A,F9.6)") "sigma (ang)     : ", sig
+  write(6,*)
+
+! Write geometry information
+
+  write(6,"(A)") "Geometry (ang)"
+  write(6,"(8X,3(A1,10X))") "x", "y", "z"
+  do i=1,natom
+    write(6,"(A2,1X,3(F10.6,1X))") atom(i), positions(i,:)
+  enddo
+  write(6,*)
+
+! Write velocities information
+
+  write(6,"(A)") "Velocities (ang/fs)"
+  write(6,"(8X,3(A1,10X))") "x", "y", "z"
+  do i=1,natom
+    write(6,"(A2,1X,3(F10.6,1X))") atom(i), velocities(i,:)
+  enddo
+
+! Conversion to au
+
+  mass = mass * amu_to_au
+  timestep = timestep * fs_to_timeau
+  sig = sig * ang_to_bohr
+  eps = eps * ev_to_Hartree
+  positions = positions * ang_to_bohr
+  velocities = velocities * ang_to_bohr / fs_to_timeau
+
+! Write input converted
+
+  write(6,*)
+  write(6,"(A)") "-------------------------------------"
+  write(6,"(A)") "Initial parameters in atomic units"
+  write(6,*) " "
+  write(6,"(A,F10.6)") "time step  : ", timestep
+  write(6,"(A,F12.6)") "masses     : ", mass
+  write(6,"(A,E13.6)") "epsilon    : ", eps 
+  write(6,"(A,F9.6)") "sigma      : ", sig
+  write(6,*)
+
+! Write geometry information
+
+  write(6,"(A)") "Geometry"
+  write(6,"(8X,3(A1,10X))") "x", "y", "z"
+  do i=1,natom
+    write(6,"(A2,1X,3(F10.6,1X))") atom(i), positions(i,:)
+  enddo
+  write(6,*)
+
+! Write velocities information
+
+  write(6,"(A)") "Velocities"
+  write(6,"(8X,3(A1,10X))") "x", "y", "z"
+  do i=1,natom
+    write(6,"(A2,1X,3(F10.6,1X))") atom(i), velocities(i,:)
+  enddo
+
+! Compute distances for LJ
+
+  call get_distances(positions, distances, natom)
+
+! Compute LJ forces
+
+  call get_forces(forces, distances, positions, sig, eps, natom)
+
+! Calculate total energy
+
+  call get_kinetic(velocities, natom, kin_ener, mass)
+  call get_potential(distances, natom, sig, eps, pot_ener)
+  energy = kin_ener + pot_ener
+  
+! Initialization of step
+
+  step = 0
+
+! Write geometry and energy on the output
+
+  time = step * timestep * timeau_to_fs
+  write(10,"(I3)") natom
+  write(10,"(A,I4,A,F6.3,3(A,E15.6))") "# step: ", step, ",  t(fs): ", time, &
+                                     & ",   E =", energy, ",   Ek =", kin_ener, ",   Ep =", pot_ener
+  do i = 1,natom
+     write(10,*) atom(i), positions(i,:)
+  enddo
+  write(11,*) "# step, time, energy, kin_ener, pot_ener"
+  write(11,*) step, time, energy, kin_ener, pot_ener
+
+! Debug
+! write(6,*) 
+! write(6,*) "step, Total Energy, Kinetic, Potential"
+! write(6,*)
+! write(6,*) step, ") ", ",   E =", energy, ",   Ek =", kin_ener, ",   Ep =", pot_ener
+
+! *****************************************************
+
+! Loop for the trajectory
+
+  do step = 1, nstep
+
+! Velocity Verlet
+
+  do i=1,natom
+    do j=1,3
+      velocities(i,j)=velocities(i,j)+0.5d0*timestep*forces(i,j)/mass
+    enddo
+  enddo
+
+  do i=1,natom
+    do j=1,3
+      positions(i,j)=positions(i,j)+timestep*velocities(i,j)
+    enddo
+  enddo
+
+  call get_distances(positions,distances,natom)
+  call get_forces(forces,distances,positions,sig,eps,natom)
+
+  do i=1,natom
+    do j=1,3
+      velocities(i,j)=velocities(i,j)+0.5d0*timestep*forces(i,j)/mass
+    enddo
+  enddo
+
+! Calculate energies
+
+  call get_kinetic(velocities, natom, kin_ener, mass)
+  call get_potential(distances, natom, sig, eps, pot_ener)
+  energy = kin_ener + pot_ener
+
+! Debug
+! write(6,*) step, ") ", ",   E =", energy, ",   Ek =", kin_ener, ",   Ep =", pot_ener
+
+! Write geometry and energy on the output
+
+  time = step * timestep * timeau_to_fs
+  write(10,"(I3)") natom                                
+  write(10,"(A,I4,A,F9.3,3(A,E15.6))") "# step: ", step, ",  t(fs): ", time, &
+                                    & ",   E =", energy, ",   Ek =", kin_ener, ",   Ep =", pot_ener
+  do i = 1, natom
+     write(10,*) atom(i), positions(i,:)
+  enddo
+
+  write(11,*) step, time, energy, kin_ener, pot_ener
+  
+  enddo
+
+! *****************************************************
+
+  close(10)
+  close(11)
+
+  end program md
---
+++ utilities.f90
@@ -0,0 +1,174 @@
+  module utilities
+
+  public::lecture_xyz             ! Read and save positions in an .xyz file
+  public::get_distances           ! Calculate square of distances between atoms
+  public::get_forces              ! Calculate forces from the positions
+  public::get_kinetic             ! Calculate kinetic energy
+  public::get_potential           ! Calculate potential energy
+
+  contains
+
+! **********************************************************
+
+  subroutine lecture (filename, positions, atom, velocities, nstep, mass, timestep, sig, eps, natom)
+
+! Record input parameters
+
+  implicit none
+
+  integer                                        :: i, j, nstep, natom
+  double precision                               :: timestep, mass, eps, sig
+  double precision, allocatable, dimension(:,:)  :: positions, velocities
+  character(len=100)                             :: filename
+  character(len=2), allocatable, dimension(:)    :: atom
+
+  open(1,file=filename,status="old")
+
+  read(1,*) 
+  read(1,*) nstep
+  
+  read(1,*)
+  read(1,*) timestep
+
+  read(1,*)
+  read(1,*) natom
+
+  ! Allocate arrays
+
+  allocate(positions(natom,3))
+  allocate(atom(natom))
+  allocate(velocities(natom,3))
+
+  read(1,*)
+  read(1,*) mass
+  
+  read(1,*)
+  do i=1,natom
+     read(1,*) atom(i), positions(i,:)
+  enddo
+
+  read(1,*)
+  read(1,*) sig
+
+  read(1,*)
+  read(1,*) eps
+
+  read(1,*)
+  do i=1,natom
+     read(1,*) velocities(i,:)
+  enddo
+
+  ! Close file
+  close(1)
+
+  endsubroutine lecture
+
+! **********************************************************
+
+  subroutine get_distances (positions, distances, natom)
+
+! Calculate distances between atoms
+
+  implicit none
+
+  integer, intent(in)           :: natom
+  double precision, intent(in)  :: positions(natom,3)
+  double precision, intent(out) :: distances(natom,natom)
+
+  integer                       :: i, j
+
+!  Initialization of the distances
+  distances=0.0d0
+
+  do i = 1, natom
+      do j = 1, natom
+        distances(i,j) = dsqrt((positions(i,1) - positions(j,1))**2 + &
+                             & (positions(i,2) - positions(j,2))**2 + &
+                             & (positions(i,3) - positions(j,3))**2)
+      enddo
+  enddo
+
+  endsubroutine get_distances
+
+! **********************************************************
+
+  subroutine get_forces(forces, distances, positions, sig, eps, natom)
+
+! Calculate LJ forces
+
+  implicit none
+
+  integer, intent(in)             :: natom
+  double precision, intent(in)    :: eps, sig
+  double precision, intent(inout) :: forces(natom,3)
+  double precision, intent(in)    :: positions(natom,3), distances(natom,natom)
+
+  integer                         :: i, j, k
+
+
+! Initialization of the forces
+  forces = 0.d0
+
+  do i = 1, natom  ! the atom experiencing the forces
+    do k = 1,3  ! xyz
+        do j = 1, natom  ! the atom generating the force
+          if (i == j) cycle  ! skip the interaction with itself
+          forces(i,k) = forces(i,k) + 48*eps*(positions(i,k)-positions(j,k)) * &
+                                    & (sig**12/distances(i,j)**14 - 0.5d0*sig**6/distances(i,j)**8) 
+        enddo
+    enddo
+  enddo
+
+  endsubroutine get_forces
+
+! **********************************************************
+
+  subroutine get_kinetic(velocities,natom,kin_ener,mass)
+
+! Calculate Kinetic energy
+
+  implicit none
+
+  double precision, intent(in)     :: velocities(natom,3), mass
+  double precision, intent(out)    :: kin_ener
+  integer, intent(in)              :: natom
+  integer                          :: i,k
+
+! Initialization of kinetic energy
+  kin_ener=0.0d0
+
+  do i=1,natom  ! iterate over all atoms
+    do k=1,3  ! iterate over x, y and z
+      kin_ener = kin_ener + 0.5d0 * mass * velocities(i,k)*velocities(i,k)
+    enddo
+  enddo
+
+  endsubroutine get_kinetic
+
+! **********************************************************
+
+  subroutine get_potential (distances,natom,sig,eps,pot_ener)
+
+! Calculate Potential energy
+
+  implicit none
+
+  double precision, intent(in)  :: distances(natom,natom), sig, eps
+  double precision, intent(out) :: pot_ener
+  integer, intent(in)           :: natom
+  integer                       :: i,j
+
+! Initialization of potential energy
+  pot_ener = 0.0d0
+
+! Indexes are set up to avoid calculating double interactions (between the same pair of atoms)
+
+  do i = 1,natom-1
+     do j = i+1,natom
+        pot_ener = pot_ener + 4.d0 * eps * ((sig/distances(i,j))**12-(sig/distances(i,j))**6)
+     enddo
+  enddo
+
+  endsubroutine get_potential
+
+end module utilities
